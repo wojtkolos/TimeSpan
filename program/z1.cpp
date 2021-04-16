750855e09c5850938242ac8f0fdaf2ad3cb4210a
Wojciech Witczak <70755745+wojtkolos@users.noreply.github.com>
2021-04-16 23:34:20 +0200

    Update z1.cpp

--- program/z1.cpp
+++ program/z1.cpp
@@ -4,16 +4,29 @@
 #include <tgmath.h>
 
 
-TimeSpan::TimeSpan(unsigned int timeInSeconds) {
+TimeSpan::TimeSpan(unsigned int timeInSeconds) 
+{
 	seconds = timeInSeconds % 60;
 	minutes = floor((timeInSeconds / 60) % 60);
 	hours = timeInSeconds / 3600;
 }
 
-int TimeSpan::getHours() const { return hours; }
-int TimeSpan::getMinutes() const { return minutes; }
-int TimeSpan::getSeconds() const { return seconds; }
+int TimeSpan::getHours() const 
+{ 
+	return hours; 
+}
 
-void TimeSpan::print() {
+int TimeSpan::getMinutes() const 
+{ 
+	return minutes; 
+}
+
+int TimeSpan::getSeconds() const 
+{ 
+	return seconds; 
+}
+
+void TimeSpan::print() 
+{
 	std::cout << std::setfill('0') << std::setw(2) << hours << " : " << minutes << " : " << seconds;
 }
